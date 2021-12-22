93bbbe10c5621421566ca6249273196b08a60327
shibaji.chatterjee <Shibaji.Chatterjee@willistowerswatson.com>
2021-12-22 13:39:35 +0530

    2nd

--- mainjenkin/src/main/java/com/jenkin/JenkinApplication.java
+++ mainjenkin/src/main/java/com/jenkin/JenkinApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinApplication.class, args);
+		SpringApplication.run(JenkinApplication.class, args);//lllll
 	}
 
 }
